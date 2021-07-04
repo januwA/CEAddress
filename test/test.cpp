fdd2ab7720cbd0caa92f5b365352efe09d32abee
januwA <ajanuw1995@gmail.com>
2021-07-04 19:56:20 +0800

    添加项目文件。

---
+++ test/test.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <CEAddress.h>
+#include <map>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+  MessageBoxA(0, "a", "b", 0);
+  try
+  {
+    map<string, LPVOID> symbols;
+    symbols["a"] = (LPVOID)2;
+    cout << ces::getAddress("1 + a", symbols) << endl;
+    cout << ces::getAddress("messageboxa", symbols) << endl;
+    cout << ces::getAddress("user32.messageboxa", symbols) << endl;
+    cout << ces::getAddress("user32.MessageBoxA", symbols) << endl;
+  }
+  catch (const std::exception& e)
+  {
+    cout << e.what() << endl;
+  }
+  cin.get();
+}
